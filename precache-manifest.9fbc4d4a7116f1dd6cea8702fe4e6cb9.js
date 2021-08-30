a11c2f0fa434b92f416c7b0aad0d8d891223f3b8
robin-cavli <robin.chacko@cavliwireless.com>
2021-08-30 14:41:43 +0530

    Updates

---
+++ precache-manifest.9fbc4d4a7116f1dd6cea8702fe4e6cb9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935948c8863f19c36908e9d35db56889",
+    "url": "/matrimony/index.html"
+  },
+  {
+    "revision": "2052832696523fed756c",
+    "url": "/matrimony/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "45eae41ec4b676ac6ddb",
+    "url": "/matrimony/static/css/main.99721e0b.chunk.css"
+  },
+  {
+    "revision": "2052832696523fed756c",
+    "url": "/matrimony/static/js/2.877588d4.chunk.js"
+  },
+  {
+    "revision": "45eae41ec4b676ac6ddb",
+    "url": "/matrimony/static/js/main.5ad3f2b0.chunk.js"
+  },
+  {
+    "revision": "120affeb04f35dcbddd7",
+    "url": "/matrimony/static/js/runtime~main.c0ada547.js"
+  },
+  {
+    "revision": "e67e358c72e30a5ec593534154b91ea6",
+    "url": "/matrimony/static/media/default-profile-man.e67e358c.jpg"
+  },
+  {
+    "revision": "10b9c9abf2618b27dec2108c28e90d7f",
+    "url": "/matrimony/static/media/default-profile-no-gender.10b9c9ab.png"
+  },
+  {
+    "revision": "201d17520f0f579146c4e7f45960dd39",
+    "url": "/matrimony/static/media/default-profile-woman.201d1752.jpg"
+  },
+  {
+    "revision": "91b1ee008fa842562785e98e71a2c77b",
+    "url": "/matrimony/static/media/heart-anim.91b1ee00.gif"
+  },
+  {
+    "revision": "208f7a6930dd1501cecfa469fddf11ad",
+    "url": "/matrimony/static/media/heart-broken.208f7a69.gif"
+  },
+  {
+    "revision": "81116db5c927cd79e3515e06ef8aedeb",
+    "url": "/matrimony/static/media/heart-loading.81116db5.gif"
+  },
+  {
+    "revision": "9885ac9d23ac8efdfd0b0272ae6e7256",
+    "url": "/matrimony/static/media/home-background-mobile.9885ac9d.jpg"
+  },
+  {
+    "revision": "bcb48aabd4454fe6c32a6845b315a814",
+    "url": "/matrimony/static/media/home-background.bcb48aab.jpg"
+  },
+  {
+    "revision": "c79f7e182f0b4097f340f7dd98d04d2f",
+    "url": "/matrimony/static/media/logo-with-name.c79f7e18.png"
+  },
+  {
+    "revision": "08ac7f5e9e0750ff62f1a0d17260cb86",
+    "url": "/matrimony/static/media/logo.08ac7f5e.png"
+  },
+  {
+    "revision": "9885ac9d23ac8efdfd0b0272ae6e7256",
+    "url": "/matrimony/static/media/man-profile-background.9885ac9d.png"
+  },
+  {
+    "revision": "9885ac9d23ac8efdfd0b0272ae6e7256",
+    "url": "/matrimony/static/media/man-woman-profile-background.9885ac9d.png"
+  },
+  {
+    "revision": "bcfdc4a378efecceea342fad686fe36e",
+    "url": "/matrimony/static/media/match-love-anim.bcfdc4a3.webp"
+  },
+  {
+    "revision": "bcb73808e0dd013dea1ed1ff2c113a78",
+    "url": "/matrimony/static/media/no-image.bcb73808.jpeg"
+  },
+  {
+    "revision": "5a84ec278d829897bd34c97c363096b5",
+    "url": "/matrimony/static/media/pre-wedding-photoshoot.5a84ec27.jpg"
+  },
+  {
+    "revision": "9885ac9d23ac8efdfd0b0272ae6e7256",
+    "url": "/matrimony/static/media/woman-profile-background.9885ac9d.png"
+  }
+]);
